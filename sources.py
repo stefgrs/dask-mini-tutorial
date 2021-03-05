a520819515fff1be0530a95d5b3b6ba20d88dc00
stefgrs <stef.grs@gmail.com>
2021-03-05 03:18:21 +0000

    added two files from official dask tutorial

---
+++ sources.py
@@ -0,0 +1,13 @@
+'''
+-----------------------------------
+### Acknowledgements
+
+Copied directly from the [dask tutorial](https://github.com/dask/dask-tutorial) that has the following copyright:
+Copyright (c) 2017-2018, Anaconda, Inc. and contributors
+All rights reserved.
+
+'''
+
+flights_url = "https://storage.googleapis.com/dask-tutorial-data/nycflights.tar.gz"
+lazy_url = "http://www.google.com"
+bag_url = "s3://dask-data/nyc-taxi/2015/yellow_tripdata_2015-01.csv"
